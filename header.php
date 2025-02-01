f66979f7cdb08b9e9b68bf4bcfba3f064878cbe4
ej8899 <ej8899@gmail.com>
2025-01-31 21:51:33 -0700

    add: rubik font

--- header.php
+++ header.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EJMEDIA.CA - Cybersecurity & App Development</title>
+    
     <link rel="stylesheet" href="https://ejmedia.ca/styles.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    
     <script>
         function updatePageTitle(newTitle) {
             document.title = newTitle + " - EJMEDIA.CA";
