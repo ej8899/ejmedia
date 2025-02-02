abcb545c100927345e0d1fbaa28ff507f810552f
ej8899 <ej8899@gmail.com>
2025-02-02 16:33:41 -0700

    add: php file template for sub pages

---
+++ z-template.php
@@ -0,0 +1,18 @@
+<?php include "./header.php"; ?>
+<script>
+  updatePageTitle("New Page Title Text");
+</script>
+
+<style>
+ 
+
+</style>
+
+<h2>Oh, there's no blog post here yet</h2>
+<p>Please check back later, we're busy writing some amazing content!</p>
+    
+
+
+
+
+<?php include "./footer.php"; ?>
